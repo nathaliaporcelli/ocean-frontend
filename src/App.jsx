51eee9cf1b2aff904a26e316abb47b346c8df6e1
Nathalia Porcelli <57419374+nathaliaporcelli@users.noreply.github.com>
2024-01-25 14:02:22 +0000

    borda card

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <div>
+    <div className='card'>
       <h2>Rick Sanchez</h2>
       <img src="http://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
     </div>
