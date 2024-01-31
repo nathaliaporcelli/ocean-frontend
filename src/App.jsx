04dd5535357161ac7be6746323f9be36d03f5814
Nathalia Porcelli <57419374+nathaliaporcelli@users.noreply.github.com>
2024-01-31 14:38:52 +0000

    integração api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 
 function App() {
   //carregamento de dados direto via codigo
- const item1 = {
-  name:'Rick Sanchez',
-  image: 'http://rickandmortyapi.com/api/character/avatar/1.jpeg'
+//  const item1 = {
+//   name:'Rick Sanchez',
+//   image: 'http://rickandmortyapi.com/api/character/avatar/1.jpeg'
 
-}
-  const item2 = {
-  name:'Beth Smith',
-  image: 'http://rickandmortyapi.com/api/character/avatar/4.jpeg'
+// }
+//   const item2 = {
+//   name:'Beth Smith',
+//   image: 'http://rickandmortyapi.com/api/character/avatar/4.jpeg'
 
-}
-const item3 = {
-  name:'Morty Smith',
-  image: 'http://rickandmortyapi.com/api/character/avatar/2.jpeg'
+// }
+// const item3 = {
+//   name:'Morty Smith',
+//   image: 'http://rickandmortyapi.com/api/character/avatar/2.jpeg'
 
-}
-const itens = [item1,item2,item3]
+// }
+// const itens = [item1,item2,item3]
 
 //carregamento de dados via API /backend
+
+const [itens,setItens] = useState([])
 async function carregarDadosApi(){
   const apiUrl = 'https://rickandmortyapi.com/api/character'
 const response = await fetch(apiUrl)
@@ -32,6 +35,9 @@ const body = await response.json()
 console.log(body)
 
 const results = body.results;
+
+//atualiza o estado itens com resultado da API
+setItens(results)
 
 //TODO enviar results para JSX exibindo um card para cada personagem
 }
